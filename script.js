8bc9611d7a17e1a24988dc28ba5f34e2c289eeb9
vatskhushi04-hue <vatskhushi04@gmail.com>
2025-08-23 16:45:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+const menuToggle = document.querySelector('.menu-toggle');
+const navMenu = document.querySelector('.nav ul');
+
+menuToggle.addEventListener('click', () => {
+  navMenu.classList.toggle('show');
+});
 
